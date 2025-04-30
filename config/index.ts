3039cd38da9a0e98969166336df82295948a9c63
Eddy Valencia <eddyvalencia8@gmail.com>
2025-04-30 13:53:35 -0500

    feat: add support for configurable SameSite attribute in session cookies

--- config/index.ts
+++ config/index.ts
@@ -2,12 +2,14 @@ import type { AppInfo } from '@/types/app'
 export const APP_ID = `${process.env.APP_ID}`
 export const API_KEY = `${process.env.API_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
+export const APP_SESSION_SAME_SITE = `${process.env.APP_SESSION_SAME_SITE}`
 export const APP_INFO: AppInfo = {
   title: 'ClickMinded',
   description: '',
   copyright: '',
   privacy_policy: 'https://www.clickminded.com/privacy/',
   default_language: 'en',
+  disable_session_same_site: APP_SESSION_SAME_SITE === 'true',
 }
 
 export const isShowPrompt = false
