7f388dc738f85e1e81484d5151bd721dc374f7a8
Eddy Valencia <eddyvalencia8@gmail.com>
2025-05-28 10:15:40 -0500

    test: add Sentry test error logging in Main component

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -68,6 +68,13 @@ const Main: FC<IMainProps> = () => {
     }
   }, [])
 
+  // Sentry Test Error
+  useEffect(() => {
+    console.log('Sentry test: Attempting to call myUndefinedFunction')
+    // @ts-expect-error Sentry test: Intentionally calling an undefined function
+    myUndefinedFunction()
+  }, [])
+
   /*
   * conversation info
   */
